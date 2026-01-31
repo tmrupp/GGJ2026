b0f8f8b36fa52257e94be6a13eda86ab2fc36087
Thomas Rupp <tmrupp@umich.edu>
2026-01-30 21:57:25 -0700

    fixed

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
 var speed = 1.0
-var direction
+var direction: Vector2
 
 func _input(_event: InputEvent) -> void:
 	direction = Input.get_vector("Left", "Right", "Forward", "Back")
 	
 func _physics_process(delta: float) -> void:
-	velocity = Vector3(direction.x, direction.y, 0.0)  * speed
+	velocity = Vector3(direction.x, 0.0, direction.y)  * speed
 	move_and_slide()
