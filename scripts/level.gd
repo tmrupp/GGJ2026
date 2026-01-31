d2ae7b13a6cbfcdc2759f9405c58704ab1d4f830
nichgaun <nichgaun@umich.edu>
2026-01-31 14:46:08 -0500

    fix vision cone detection

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,5 +2,10 @@ extends Node3D
 
 @onready var player = $Player
 
+signal caught
+
 func _ready() -> void:
-	pass
+	caught.connect(_on_player_caught)
+
+func _on_player_caught():
+	print("we caught the player")
--- scripts/vision_cone.gd
+++ scripts/vision_cone.gd
@@ -3,10 +3,7 @@ extends Node3D
 @onready var collider = $Area3D
 @onready var level = $/root/Main
 
-signal caught
-
 func _ready() -> void:
 	collider.connect("body_entered", func(body):
-		print("body entered, checking if it's player")
 		if body == level.player:
-			caught.emit())
+			level.caught.emit())
