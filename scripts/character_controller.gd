64599be61162aaee92a0587ec981f2ba6703d326
Thomas Rupp <tmrupp@umich.edu>
2026-01-31 09:30:13 -0700

    camera rotation and charater movement based on that shit

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,11 +1,18 @@
 extends CharacterBody3D
 
-var speed = 1.0
+var speed = 5.0
+var turn_speed = .05
+
 var direction: Vector2
+var turn: float
+@onready var cam = $Camera3D
 
 func _input(_event: InputEvent) -> void:
 	direction = Input.get_vector("Left", "Right", "Forward", "Back")
+	turn = Input.get_axis("CounterClockwise", "Clockwise")
+	
 	
 func _physics_process(delta: float) -> void:
-	velocity = Vector3(direction.x, 0.0, direction.y)  * speed
+	velocity = Vector3(direction.x, 0.0, direction.y).rotated(Vector3.UP, rotation.y) * speed
+	rotate(Vector3(0, 1, 0), turn*turn_speed)
 	move_and_slide()
