8d5fed04d41b2696351c67646ca85ac60a0fc124
nichgaun <nichgaun@umich.edu>
2026-01-31 16:46:12 -0500

    vision cone wall check

--- scripts/vision_cone.gd
+++ scripts/vision_cone.gd
@@ -2,8 +2,12 @@ extends Node3D
 
 @onready var collider = $Area3D
 @onready var level = $/root/Main
+@onready var raycast = $RayCast3D
 
 func _ready() -> void:
 	collider.connect("body_entered", func(body):
 		if body == level.player:
-			level.caught.emit())
+			raycast.look_at(level.player.position)
+			raycast.force_raycast_update()
+			if raycast.get_collider() == level.player:
+				level.caught.emit())
