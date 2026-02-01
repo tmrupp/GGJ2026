31aa5811a4c3fdc7b27e6082d96a6c659ec52bb0
Thomas Rupp <tmrupp@umich.edu>
2026-01-31 21:29:03 -0700

    maksiughbn

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -1,6 +1,7 @@
 @tool
 extends Area3D
 
+@onready var masks = $".."
 @export var color = Color.RED
 @export var texture: Texture
 
@@ -8,10 +9,15 @@ func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		$Sprite3D.texture = texture
 
+func setup(color, texture):
+	$Sprite3D.texture = texture
+	color = color
+
 func _on_entered(body):
 	if body.name == "Player":
 		body.mask_up(color, $Sprite3D.texture)
-	queue_free()
+	#queue_free()
+	masks.remove_mask(self)
 
 func _ready() -> void:
 	connect("body_entered", _on_entered)
